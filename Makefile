49e00517f705a99e7357f307bb8420fc401712c3
Ismael Montero Ianez <ismonter@c1r2s4.42madrid.com>
2026-01-28 21:12:13 +0100

    segunda subida 28 enero

--- Makefile
+++ Makefile
@@ -6,23 +6,64 @@
 #    By: ismonter <ismonter@student.42madrid.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2026/01/19 11:38:10 by ismonter          #+#    #+#              #
-#    Updated: 2026/01/26 16:10:04 by ismonter         ###   ########.fr        #
+#    Updated: 2026/01/28 21:04:05 by ismonter         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME = libft.a
-# SOURCE = ft_memcpy.c ft_strdup.c
-SOURCE = $(wildcard *.c)
+SOURCE = \
+	ft_isalpha.c \
+	ft_isdigit.c \
+	ft_isalnum.c \
+	ft_isascii.c \
+	ft_isprint.c \
+	ft_strlen.c \
+	ft_memset.c \
+	ft_bzero.c \
+	ft_memcpy.c \
+	ft_memmove.c \
+	ft_strlcpy.c \
+	ft_strlcat.c \
+	ft_toupper.c \
+	ft_tolower.c \
+	ft_strchr.c \
+	ft_strrchr.c \
+	ft_strncmp.c \
+	ft_memchr.c \
+	ft_memcmp.c \
+	ft_strnstr.c \
+	ft_atoi.c \
+	ft_calloc.c \
+	ft_strdup.c \
+	ft_substr.c \
+	ft_strjoin.c \
+	ft_strtrim.c \
+	ft_split.c \
+	ft_itoa.c \
+	ft_strmapi.c \
+	ft_striteri.c \
+	ft_putchar_fd.c \
+	ft_putstr_fd.c \
+	ft_putendl_fd.c \
+	ft_putnbr_fd.c \
+	ft_lstnew.c \
+	ft_lstadd_front.c \
+	ft_lstsize.c \
+	ft_lstlast.c \
+	ft_lstadd_back.c \
+	ft_lstdelone.c \
+	ft_lstclear.c \
+	ft_lstiter.c \
+	ft_lstmap.c
 OBJS = $(SOURCE:.c=.o)
 
 CFLAGS = -Wall -Wextra -Werror
-bonus: all
 all: $(NAME)
 
 $(NAME): $(OBJS)
 	ar rcs $(NAME) $(OBJS)
 
-%.o: %.c
+%.o: %.c libft.h
 	cc $(CFLAGS) -c $< -o $@
 
 clean:
--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,11 +6,24 @@
 /*   By: ismonter <ismonter@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/26 19:33:36 by ismonter          #+#    #+#             */
-/*   Updated: 2026/01/28 17:41:34 by ismonter         ###   ########.fr       */
+/*   Updated: 2026/01/28 20:52:42 by ismonter         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	*del_first_node(t_list *new_content, void (*del)(void *))
+{
+	del(new_content);
+	return (NULL);
+}
+
+void	*del_list(t_list *new_content, t_list *start_lst, void (*del)(void *))
+{
+	del(new_content);
+	ft_lstclear(&start_lst, del);
+	return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -22,11 +35,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 		return (NULL);
 	new_content = f(lst->content);
 	new_lst = ft_lstnew(new_content);
-	if (new_lst == NULL || new_content == NULL)
-	{
-		del(new_content);
-		free(new_content);
-	}
+	if (new_lst == NULL)
+		return (del_first_node(new_content, del));
 	start_lst = new_lst;
 	lst = lst->next;
 	new_lst = new_lst->next;
@@ -34,21 +44,10 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	{
 		new_content = f(lst->content);
 		new_lst = ft_lstnew(new_content);
-		if (new_lst == NULL || new_content == NULL)
-		{
-			ft_lstclear(&start_lst, del);
-			return (NULL);
-		}
+		if (new_lst == NULL)
+			return (del_list(new_content, start_lst, del));
 		lst = lst->next;
 		ft_lstadd_back(&start_lst, new_lst);
 	}
 	return (start_lst);
 }
-/*
-int	main(void)
-{
-
-
-
-}
-*/
